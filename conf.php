b7965afb243cd686ce2235de9bd8bf2d2b0bb6c8
Iyamoto <Iyamoto@users.noreply.github.com>
2013-09-13 15:18:40 +0400

    new git rep

---
+++ conf.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * Config file for PHP-NetFlow
+ */
+
+$netflow_base_dir = '/var/cache/nfdump/flows/live/4217FB546AC8F2E170FB0D58286FF0DD';
+$netflow_current_dir = '2013-09-13';//TODO add date detection
+$netflow_last_file = 'nfcapd.1';//FIXME add last file detection 
+
+$nfdump = '/usr/bin/nfdump';
+
+//NFDump filters
+$lan_src = 'src net 10.0/8 or src net 192.168/16';
+$lan_dst = 'dst net 10.0/8 or dst net 192.168/16';
+?>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * NetFlow anomaly detector
+ * Finds malware/virus bots in LAN
+ */
+
+mb_internal_encoding("UTF-8");
+require_once 'conf.php';
+echo "\n[+] Started\n";
+
+
+?>
