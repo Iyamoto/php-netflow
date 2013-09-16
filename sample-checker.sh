9856afd1a39c6911dfd1a9653877605e229c2628
Iyamoto <Iyamoto@users.noreply.github.com>
2013-09-16 12:47:15 +0400

    comments to sample-checker.sh

--- sample-checker.sh
+++ sample-checker.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#dont foget to do: chmod +x sample-checker.sh
+#replace basedir with your path to php-netflow
+#and add to /etc/crontab: */10 * * * * root /path/to/sample-checker.sh >/dev/null 2>&1
 basedir="/root/netflow/php-netflow"
 cd $basedir
 date > lastrun.log
