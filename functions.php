82ee7e45dd6b524a4edb93cb9a59ea293060449d
Iyamoto <Iyamoto@users.noreply.github.com>
2013-09-17 10:47:00 +0400

    today

--- functions.php
+++ functions.php
@@ -165,6 +165,7 @@ function build_html_page($daily_file, $page_path, $page_filename, $tpl_name) {
         $html_blocks = preg_replace('|<hr>$|', '', $html_blocks);
         $html = str_replace('$blocks', $html_blocks, $html_index_tpl);
         $today = date_from_filename($page_filename);
+        if($today=='index') $today = date("Y-m-d");//FIXME
         $html = str_replace('$today', $today, $html);
         $where_to_write = $page_path . DIRECTORY_SEPARATOR . $page_filename;
         if (file_put_contents($where_to_write, $html)) {
