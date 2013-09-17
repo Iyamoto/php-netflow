ee20ea486a81fd646041276390b8f8ac32d4cb24
Iyamoto <Iyamoto@users.noreply.github.com>
2013-09-17 13:22:46 +0400

    169.254/16

--- sample-config.php
+++ sample-config.php
@@ -22,7 +22,7 @@ $nfdump = '/usr/bin/nfdump';//which nfdump
 
 //NFDump filters
 $lan_src = 'src net 10.0/8 or src net 192.168/16';
-$lan_dst = 'dst net 10.0/8 or dst net 192.168/16';
+$lan_dst = 'dst net 10.0/8 or dst net 192.168/16 or dst net 169.254/16';
 
 //What are we looking for?
 //https://isc.sans.edu/trends.html
