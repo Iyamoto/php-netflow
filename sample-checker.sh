008cac6f7407558e13df9f0bbc8b0423ddb61abf
Iyamoto <Iyamoto@users.noreply.github.com>
2013-09-17 12:39:54 +0400

    archive check

--- sample-checker.sh
+++ sample-checker.sh
@@ -4,8 +4,16 @@
 #replace basedir with your path to php-netflow
 #and add to /etc/crontab: */10 * * * * root /path/to/checker.sh >/dev/null 2>&1
 basedir="/root/netflow/php-netflow"
+archiveindex="/var/www/botolovka/archive/index.html"
 cd $basedir
 date > lastrun.log
 /usr/bin/php checker.php >> lastrun.log
 /usr/bin/php filter.php >> lastrun.log
 /usr/bin/php reporter.php >> lastrun.log
+
+archivedate=`stat -c%y $archiveindex | gawk '{ print $1 }'`
+today=`date +%Y-%m-%d`
+if [[ "$archivedate" == "$today" ]]
+then echo "[+] No need to acrchive" >> lastrun.log
+else /usr/bin/php archive.php >> lastrun.log
+fi
