7662b409e4cbab88b703e35efbc08e8a9e8982e4
Iyamoto <Iyamoto@users.noreply.github.com>
2013-09-19 13:17:25 +0400

    fix

--- stats.php
+++ stats.php
@@ -39,8 +39,10 @@ for ($day_counter = 0; $day_counter < $stats_deep; $day_counter++) {
     echo "[+] Processing file: $file_names[$day_counter]\n";
     $day = date_from_filename($file_names[$day_counter]);
     $html_block_day = str_replace('$day', $day, $html_block_tpl);
-    if($day_counter==0) $archlink = '../index.html';
-    else $archlink = '../archive/archive'.$day.'.html';
+    if ($day_counter == 0)
+        $archlink = '../index.html';
+    else
+        $archlink = '../archive/archive' . $day . '.html';
     $html_block_day = str_replace('$archlink', $archlink, $html_block_day);
 
     $daily_db_file = $tmp_dir . DIRECTORY_SEPARATOR . $file_names[$day_counter];
@@ -49,9 +51,9 @@ for ($day_counter = 0; $day_counter < $stats_deep; $day_counter++) {
         $daily_size = sizeof($daily);
         echo "[+] Read $daily_size daily blocks\n";
         foreach ($daily as $ip => $types) {
-            foreach ($types as $type => $evidences) {
-                $top_ip[$ip] = sizeof($evidences);
-            }
+            $top_ip[$ip] = 0;
+            foreach ($types as $type => $evidences)
+                $top_ip[$ip] += sizeof($evidences);
         }
         arsort($top_ip);
         $ip_counter = 0;
