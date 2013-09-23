5b7fc77450f8830bcb8c4a0bbe939a29f07f1cba
Iyamoto <Iyamoto@users.noreply.github.com>
2013-09-23 10:07:10 +0400

    more w

--- more.php
+++ more.php
@@ -4,12 +4,30 @@
  *   More info about an IP
  */
 $exec_time = microtime(true);
-require_once 'config.php';
 echo "\n[+] Started\n";
 
 $ip = htmlentities($_GET['ip'], ENT_QUOTES, 'UTF-8');
+$last_daily = get_lastmodified_file('tmp');
 echo $ip;
+var_dump($last_daily);
 
 $exec_time = round(microtime(true) - $exec_time, 2);
 echo "[i] Execution time: $exec_time sec.\n";
+
+function get_lastmodified_file($dir) {
+    $files = array();
+    if ($handle = opendir($dir)) {
+        while (false !== ($file = readdir($handle))) {
+            if ($file != "." && $file != "..") {
+                $files[filemtime($dir . DIRECTORY_SEPARATOR . $file)] = $file;
+            }
+        }
+        closedir($handle);
+        ksort($files);
+        $reallyLastModified = end($files);
+        return $reallyLastModified;
+    }
+    else
+        return false;
+}
 ?>
