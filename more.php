c02f1f17677b3dbc92b6db523eae26d468474ffe
Iyamoto <Iyamoto@users.noreply.github.com>
2013-09-23 10:20:11 +0400

    more 4

--- more.php
+++ more.php
@@ -8,7 +8,7 @@ echo "<br>[+] Started<br>";
 
 $ip = htmlentities($_GET['ip'], ENT_QUOTES, 'UTF-8');
 
-echo $ip.'<br>';
+echo $ip . '<br>';
 $daily_files = get_dir_list('db');
 $daily_files_size = sizeof($daily_files);
 if ($daily_files_size < 1)
@@ -18,13 +18,15 @@ foreach ($daily_files as $daily_file) {
         $file_names[] = $daily_file;
 }
 rsort($file_names);
-
-var_dump($file_names[0]);
+$daily = read_db_from_file($file_names[0]);
+if ($daily) { //Daily db exists
+    $daily_size = sizeof($daily);
+    echo "[+] Read $daily_size daily blocks<br>";
+    var_dump($daily);
+}
 
 $exec_time = round(microtime(true) - $exec_time, 2);
 echo "[i] Execution time: $exec_time sec.<br>";
-
-
 
 function get_dir_list($dir) {
     $files = array();
@@ -39,5 +41,30 @@ function get_dir_list($dir) {
     }
     else
         return false;
+}
+
+function read_db_from_file($filename) {
+    if (file_exists($filename)) {
+        $json = load_json($filename);
+        if ($json)
+            return $json;
+        else
+            return false;
+    } else {
+        echo "[-] $filename not found\n";
+        return false;
+    }
+}
+
+function load_json($fn) {
+    $gz = file_get_contents($fn);
+    if ($gz) {
+        $json = gzuncompress($gz);
+        $data = json_decode($json, true);
+        return $data;
+    } else {
+        echo "[-] Cant load file $fn\n";
+        return false;
+    }
 }
 ?>
